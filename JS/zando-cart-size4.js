b0608c6b3f75147e9137165edd14331ca8694108
ManetShelby <vamanet4@gmail.com>
2025-04-25 13:45:05 +0700

    news

--- JS/zando-cart-size4.js
+++ JS/zando-cart-size4.js
@@ -1,4 +1,3 @@
-// Quantity
 let btnd = document.getElementById("btnd");
         let btnN = document.getElementById("btnN");
         let btnI = document.getElementById("btnI");
@@ -51,4 +50,45 @@ function fnimg3() {
     img2.className = "";
     img3.className = "imaborder";
 }
-
+//zando-cart4
+let btnxs = document.getElementById("btnxs");
+let btns = document.getElementById("btns");
+let btnm = document.getElementById("btnm");
+let btnl = document.getElementById("btnl");
+let btnxl = document.getElementById("btnxl");
+function fnxs() {
+    btnxs.className = "border";
+    btns.className = "size";
+    btnm.className = "size";
+    btnl.className = "size";
+    btnxl.className = "size";
+}
+function fns() {
+    btnxs.className = "size";
+    btns.className = "border";
+    btnm.className = "size";
+    btnl.className = "size";
+    btnxl.className = "size";
+}
+function fnm() {
+    btnxs.className = "size";
+    btns.className = "size";
+    btnm.className = "border";
+    btnl.className = "size";
+    btnxl.className = "size";
+}
+function fnl() {
+    btnxs.className = "size";
+    btns.className = "size";
+    btnm.className = "size";
+    btnl.className = "border";
+    btnxl.className = "size";
+}
+function fnxl() {
+    btnxs.className = "size";
+    btns.className = "size";
+    btnm.className = "size";
+    btnl.className = "size";
+    btnxl.className = "border";
+}
+// rotine-cart1
